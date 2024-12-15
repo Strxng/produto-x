a79014d2f294b8e8f524923a8981f00dba478cb6
Leo Bazan <leonardo.picinin@ciss.com.br>
2024-12-15 15:19:55 -0300

    useQRCodeScan hook

---
+++ hooks/useQRCodeScan.ts
@@ -0,0 +1,37 @@
+import { CONSTANTS } from "@/configs/contants";
+import { BarcodeScanningResult } from "expo-camera";
+import { useCallback, useEffect, useState } from "react";
+import { Alert } from "react-native";
+
+interface IUseQRCodeScanParams {
+  onScanCorrect: () => void;
+}
+
+export const useQRCodeScan = ({ onScanCorrect }: IUseQRCodeScanParams) => {
+  const [scannedCode, setScannedCode] = useState<string>("");
+
+  const onScan = useCallback(
+    (scanningResult: BarcodeScanningResult) => {
+      if (!scannedCode && scanningResult.data !== scannedCode) {
+        setScannedCode(scanningResult.data);
+      }
+    },
+    [scannedCode]
+  );
+
+  useEffect(() => {
+    if (scannedCode && scannedCode !== CONSTANTS.qrcodeKey) {
+      Alert.alert(
+        "QRCode inválido",
+        "O QRCode lido é incorreto, leia outro por favor.",
+        [{ text: "Entendi", onPress: () => setScannedCode("") }]
+      );
+    }
+
+    if (scannedCode && scannedCode === CONSTANTS.qrcodeKey) {
+      onScanCorrect();
+    }
+  }, [scannedCode, setScannedCode, Alert]);
+
+  return { onScan };
+};
